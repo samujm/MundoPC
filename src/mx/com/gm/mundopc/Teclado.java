6eb87bb2dfba2d8977d855d55bd8625acaf069bd
Samantha González <samujm76@gmail.com>
2022-08-15 23:19:16 -0500

    Creación de la clase Teclado

---
+++ src/mx/com/gm/mundopc/Teclado.java
@@ -0,0 +1,15 @@
+package mx.com.gm.mundopc;
+
+public class Teclado extends DispositivoEntrada{
+    private final int idTeclado;
+    private static int contadorTeclados;
+    
+    public Teclado(String tipoEntrada, String marca){
+        super(tipoEntrada, marca);
+        this.idTeclado = ++Teclado.contadorTeclados;        
+    }
+    
+    public String toString(){
+        return "Teclado{" + "idTeclado=" + idTeclado + ", " + super.toString() + '}';
+    }
+}
